c8a895fb1d78f3f5c390865c1aca0e7bf7892da9
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-12-28 22:16:26 +0530

    enemy added

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import random
 
 # setting up the path
 print(os.getcwd())
@@ -15,28 +16,58 @@ pygame.display.set_icon(ico)
 
 # adding the player
 playerImg = pygame.image.load('./Music_box/icons/search.ico')
+#  initial coordinates of the player
 pX = 370
 pY = 480
+pX_change = 0
 
 
-def player():
-    screen.blit(playerImg, (pX, pY))
+def player(x, y):
+    if x > 736:
+        x = 736
+    if x < 0:
+        x = 0
+    screen.blit(playerImg, (x, y))
+
+
+# loading the enemy
+enemyImg = pygame.image.load('./Music_box/icons/document.ico')
+#  initial coordinates of the player
+eX = random.randint(0, 736)
+eY = random.randint(50, 350)
+eX_change = 0
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 
 #  adding the event for quiting the window
 running = True
 #  gaming loop
 while running:
-    for event in pygame.event.get():   # event.get will get all event happening
+    for event in pygame.event.get():  # event.get will get all event happening
         if event.type == pygame.QUIT:  # pygame QUITE is an event happens when the close button is clicked
             running = False
+        #         keystroke event
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                print("key left pressed")
+                pX_change = -0.1
+            if event.key == pygame.K_RIGHT:
+                print("key right pressed")
+                pX_change = 0.1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                print("KEY released..")
+                pX_change = 0
 
-#     anything stays all time should be here
-#     filling the screen at the beginning
+    #     anything stays all time should be here
+    #     filling the screen at the beginning
 
     screen.fill((0, 128, 128))
-    player()
-#     updating the window
+    pX = pX + pX_change
+    player(pX, pY)
+    enemy(eX, eY)
+    #     updating the window
     pygame.display.update()
-
-
