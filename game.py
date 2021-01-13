200c794739b145d68b552e3b42781cecfc0cde64
AkhilVinayakp <nirvanaosborn@gmail.com>
2021-01-13 22:39:14 +0530

    redesigned player

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pygame
-import os
+import random
 
 # initializing pygame
 pygame.init()
@@ -18,20 +18,30 @@ class Character:
 
 
 class Player(Character):
-    def __init__(self):
-        image = pygame.image.load("./Music_box/icons/search.ico")
-        super().__init__(image)
+    def __init__(self, image=pygame.image.load("./Music_box/icons/search.ico"), pos=(370, 480)):
+        super().__init__(image, pos)
 
     def move(self, x, y):
         if x < 0:
-            x = 0
-        elif x > 756:
             x = 756
+        elif x > 756:
+            x = 0
+        if y < 0:
+            y = 556
+        elif y > 556:
+            y = 0
         super().move_ch(x, y)
         return x, y
 
 
+class Box(Character):
+    def __init__(self):
+        image = pygame.image.load("./Music_box/icons/my_computer.ico")
+        super().__init__(image, pos=(230, 280))
+
+
 pablo = Player()
+p2 = Player(pygame.image.load("./Music_box/icons/folder.ico"), pos=(400, 540))
 # creating the title and icon
 pygame.display.set_caption("music box")
 icon = pygame.image.load("./Music_box/icons/document.ico")
@@ -42,9 +52,11 @@ px_c = 0
 py_c = 0
 # creating game loop
 running = True
+gen = False
+l = [.5, -.5, .3, -.2, -.9, .9, .7, -.7]
 while running:
     # cdf
-    screen.fill((0, 127, 129))
+    # screen.fill((0, 127, 129))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -63,12 +75,19 @@ while running:
             if event.key == pygame.K_DOWN:
                 print('down')
                 py_c = 0.1
+            if event.key == pygame.K_q:
+                gen = False
+            if event.key == pygame.K_SPACE:
+                gen = True
         if event.type == pygame.KEYUP:
             if event.key in[pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]:
                 px_c = 0
                 py_c = 0
-
+    if gen:
+        px_c = random.choice(l)
+        py_c = random.choice(l)
     pX = pX + px_c
     pY = pY + py_c
     pX, pY = pablo.move(pX, pY)
+    pX, pY = p2.move(pX, pY)
     pygame.display.update()
